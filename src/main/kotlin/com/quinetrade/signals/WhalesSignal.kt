933bbf62c5c11f898d3450bd4c9098a8abeecc59
Arsentii Karpov <arsengizer@yandex.ru>
2025-12-01 21:05:24 +0300

    add sliding window to whale signal

--- src/main/kotlin/com/quinetrade/signals/WhalesSignal.kt
+++ src/main/kotlin/com/quinetrade/signals/WhalesSignal.kt
@@ -1,43 +1,44 @@
 package com.quinetrade.signals
 
 import com.quinetrade.sources.binance.stream.AggTradeStreamSource
-import kotlinx.coroutines.flow.channelFlow
-import kotlinx.coroutines.flow.flow
+import com.quintrade.sources.binance.data.AggTrade
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.channelFlow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 
 class WhalesSignal(val aggTrade: AggTradeStreamSource) {
 
-
-  fun observe() = flow<Int> {
+  fun observe() = flow<Double> {
     val coroutineScope = CoroutineScope(Dispatchers.IO)
     coroutineScope.launch {
       aggTrade.observeStream().collect {
-
+        if (it.isWhaleSell()) {
+          emit(it.aggTrade!!.q.toDouble()) //TODO: error handling
+        }
       }
     }
-    
   }
-  /*
-   Sliding window, e.g. 1–5 minutes.
 
-Track:
+  fun <T> Flow<T>.slidingWindowFast(windowSize: Int): Flow<List<T>> =
+    channelFlow {
+      val buffer = ArrayDeque<T>(windowSize)
+      collect { value ->
+        if (buffer.size == windowSize)
+          buffer.removeFirst()
+        buffer.addLast(value)
+        send(buffer.toList()) // копия наружу
+      }
+    }
 
-sum of notional buys
+}
 
-sum of notional sells
+fun AggTrade.isWhaleSell(thresholdUsd: Double = 100_000.0): Boolean {
+  val price = aggTrade!!.p.toDouble()
+  val qty = aggTrade!!.q.toDouble()
+  val quote = price * qty
 
-count of trades ≥ some per-trade threshold
-
-Trigger “whale event” when:
-
-total buy notional in window ≥ X, or
-
-total sell notional in window ≥ Y, or
-
-N whale trades within M seconds
-
-This catches a whale splitting orders into many chunks.
-*/
+  return aggTrade!!.m && quote >= thresholdUsd
 }
